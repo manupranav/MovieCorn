338cfb0d6618e58eed9e6a444cacbe8c4d836d16
manupranav <62414791+manupranav@users.noreply.github.com>
2022-08-21 19:22:16 -0700

    Updated logo and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+import SearchIcon from "./Search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=a959de06";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieCorn</h1>
+      <div className="search">
+        <input
+          placeholder="Search your movies.."
+          value={searchTerm}
+          onChange={(a) => setSearchTerm(a.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search-icon"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
